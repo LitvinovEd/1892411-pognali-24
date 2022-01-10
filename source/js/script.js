6ceeabee1d70d5391ff0281bb3d4ff22ec8e2b3e
LitvinovEd <lit.eduard@gmail.com>
2022-01-10 09:28:54 +0200

    5.8. Декоративные элементы внутренних страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,16 @@
 <script>
-  var navMain = document.querySelector('.main-navigation');
-  var navToggle = document.querySelector('.main-navigation__toggle');
+var navMain = document.querySelector('.main-navigation');
+var navToggle = document.querySelector('.main-navigation__toggle');
 
-  navMain.classList.remove('main-navigation--nojs');
+navMain.classList.remove('main-navigation--nojs');
 
-  navToggle.addEventListener('click', function() {
-    if (navMain.classList.contains('main-navigation--closed')) {
-      navMain.classList.remove('main-navigation--closed');
-      navMain.classList.add('main-navigation--opened');
-    } else {
-      navMain.classList.add('main-navigation--closed');
-      navMain.classList.remove('main-navigation--opened');
-    }
-  });
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
 </script>
